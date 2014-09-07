74f4af48185a2b77460c7b0f5fb3cb53a039fc76
Patrick Conley <patrick.bj.conley@gmail.com>
2014-09-07 14:58:13 -0700

    Library activity now displays a list of track IDs
    Removed the unit test referencing the activity's old text

--- vamp/src/pconley/vamp/LibraryActivity.java
+++ vamp/src/pconley/vamp/LibraryActivity.java
@@ -1,16 +1,33 @@
 package pconley.vamp;
 
+import java.util.List;
+
+import pconley.vamp.db.TrackDAO;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+/**
+ * Main activity, showing the contents of the library.
+ */
 public class LibraryActivity extends Activity {
+
+	private ListView trackListView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_library);
+
+		trackListView = (ListView) findViewById(R.id.track_list);
+
+		new LoadTrackListTask().execute();
 	}
 
 	@Override
@@ -30,5 +47,45 @@ public class LibraryActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	/*
+	 * Load the contents of the library into a TextView with execute(). Work is
+	 * done in a background thread; the task displays a progress bar wihle
+	 * working.
+	 */
+	private class LoadTrackListTask extends
+			AsyncTask<Void, Void, List<Long>> {
+
+		private ProgressDialog progress;
+
+		public LoadTrackListTask() {
+			progress = new ProgressDialog(LibraryActivity.this);
+		}
+
+		@Override
+		protected List<Long> doInBackground(Void... params) {
+			return new TrackDAO(LibraryActivity.this).getIds();
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progress.setMessage("Loading library");
+			progress.show();
+		}
+
+		protected void onPostExecute(List<Long> ids) {
+			if (progress.isShowing()) {
+				progress.dismiss();
+			}
+
+			ArrayAdapter<Long> adapter = new ArrayAdapter<Long>(
+					LibraryActivity.this, R.layout.track_list_item,
+					R.id.track_list_item, ids);
+
+			trackListView.setAdapter(adapter);
+		}
+
 	}
 }
