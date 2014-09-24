55b955cd4b9911a80435e53d9da8f526e68720ba
Patrick Conley <patrick.bj.conley@gmail.com>
2014-09-23 20:54:06 -0700

    Progress bar follows track; can seek

--- vamp/src/pconley/vamp/PlayerService.java
+++ vamp/src/pconley/vamp/PlayerService.java
@@ -8,6 +8,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class PlayerService extends Service implements
@@ -15,16 +16,48 @@ public class PlayerService extends Service implements
 		MediaPlayer.OnErrorListener, MediaPlayer.OnInfoListener {
 
 	/**
-	 * Intent action. Start playing a new track. Specify the track with the
-	 * Intent's data.
+	 * Action for incoming intents. Start playing a new track. Specify the track
+	 * with the Intent's data.
 	 */
 	public static final String ACTION_PLAY = "pconley.vamp.PlayerService.play";
 
 	/**
-	 * Intent action. Play/pause the current track. Does nothing if there is no
-	 * track in progress.
+	 * Action for incoming intents. Play/pause the current track. Does nothing
+	 * if there is no track in progress.
 	 */
 	public static final String ACTION_PLAY_PAUSE = "pconley.vamp.playerService.playPause";
+
+	/**
+	 * Action for incoming intents. Seek within the current track. Does nothing
+	 * if there is no track in progress.
+	 */
+	public static final String ACTION_SEEK = "pconley.vamp.playerService.seek";
+	public static final String EXTRA_SEEK_POSITION = "pconley.vamp.playerService.seek.time";
+
+	/**
+	 * Intent filter for outgoing broadcasts. Current track's metadata,
+	 * play/pause state, and elapsed/total time.
+	 */
+	public static final String FILTER_PROGRESS = "pconley.vamp.playerService.progress";
+
+	/**
+	 * In outgoing broadcasts, what's changed about the player's state. Done
+	 * state is only sent if there are no more tracks queued.
+	 */
+	public static final String EXTRA_PROGRESS_STATE = "pconley.vamp.playerService.progress.state";
+	public static final int PROGRESS_STATE_PAUSED = -1;
+	public static final int PROGRESS_STATE_PLAYING = 1;
+	public static final int PROGRESS_STATE_DONE = 0;
+
+	/**
+	 * In outgoing broadcasts, the current track's current position.
+	 */
+	public static final String EXTRA_PROGRESS_POSITION = "pconley.vamp.playerService.progress.position";
+
+	/**
+	 * In outgoing broadcasts, the current track's duration.
+	 */
+	public static final String EXTRA_PROGRESS_DURATION = "pconley.vamp.playerService.progress.duration";
 
 	private MediaPlayer player = null;
 
@@ -33,7 +66,6 @@ public class PlayerService extends Service implements
 	 */
 	@Override
 	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -62,6 +94,10 @@ public class PlayerService extends Service implements
 			playPause();
 			break;
 
+		case ACTION_SEEK:
+			seekTo(intent.getIntExtra(EXTRA_SEEK_POSITION, 0));
+			break;
+
 		default:
 			Log.w("Player", "Invalid action " + intent.getAction());
 			break;
@@ -72,12 +108,15 @@ public class PlayerService extends Service implements
 
 	@Override
 	public void onPrepared(MediaPlayer mp) {
-		player.start();
-		Log.d("Player", "started");
+		playPause();
 	}
 
 	@Override
 	public void onCompletion(MediaPlayer mp) {
+		Intent intent = new Intent(FILTER_PROGRESS).putExtra(
+				EXTRA_PROGRESS_STATE, PROGRESS_STATE_DONE);
+		LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+
 		player.release();
 		player = null;
 	}
@@ -129,14 +168,48 @@ public class PlayerService extends Service implements
 	private void playPause() {
 		if (player == null) {
 			Log.w("Player", "Can't play/pause: no player");
+			return;
 		}
-		if (player != null && player.isPlaying()) {
+
+		Intent broadcastIntent = new Intent(FILTER_PROGRESS).putExtra(
+				EXTRA_PROGRESS_POSITION, player.getCurrentPosition()).putExtra(
+				EXTRA_PROGRESS_DURATION, player.getDuration());
+
+		if (player.isPlaying()) {
 			player.pause();
+			broadcastIntent.putExtra(EXTRA_PROGRESS_STATE,
+					PROGRESS_STATE_PAUSED);
 			Log.d("Player", "paused");
-		} else if (player != null) {
+		} else {
 			player.start();
+			broadcastIntent.putExtra(EXTRA_PROGRESS_STATE,
+					PROGRESS_STATE_PLAYING);
 			Log.d("Player", "started");
 		}
+
+		LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
+
+	}
+
+	// Seek within the current track. Works in either play/pause states.
+	private void seekTo(int time) {
+		if (player == null) {
+			return;
+		}
+
+		player.seekTo(time);
+
+		// Broadcast the current position to reset the timer
+		Intent broadcastIntent = new Intent(FILTER_PROGRESS)
+				.putExtra(EXTRA_PROGRESS_POSITION, player.getCurrentPosition())
+				.putExtra(EXTRA_PROGRESS_DURATION, player.getDuration())
+				.putExtra(
+						EXTRA_PROGRESS_STATE,
+						player.isPlaying() ? PROGRESS_STATE_PLAYING
+								: PROGRESS_STATE_PAUSED);
+
+		LocalBroadcastManager.getInstance(this).sendBroadcast(broadcastIntent);
+
 	}
 
 }
