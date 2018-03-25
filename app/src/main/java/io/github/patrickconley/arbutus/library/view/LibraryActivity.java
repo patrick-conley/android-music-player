adf7fce921fc5f8bae0bd29f4bf72e77254a26fe
Patrick Conley <patrick.bj.conley@gmail.com>
2018-03-25 14:25:38 -0700

    AMP-320: Add a trivial library

--- app/src/main/java/io/github/patrickconley/arbutus/library/view/LibraryActivity.java
+++ app/src/main/java/io/github/patrickconley/arbutus/library/view/LibraryActivity.java
@@ -1,4 +1,4 @@
-package io.github.patrickconley.arbutus.player;
+package io.github.patrickconley.arbutus.library.view;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,41 +7,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import io.github.patrickconley.arbutus.R;
 import io.github.patrickconley.arbutus.settings.view.SettingsActivity;
 
-public class PlayerActivity extends AppCompatActivity {
+public class LibraryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
-        setSupportActionBar((Toolbar) findViewById(R.id.player_toolbar));
+        setContentView(R.layout.activity_library);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.library_toolbar));
 
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
     }
 
-    public void onClickPrev(View view) {
-        // stub
-    }
-
-    public void onClickNext(View view) {
-        // stub
-    }
-
-    public void onClickPlayPause(View view) {
-        // stub
-    }
-
-    public void onClickSeek(View view) {
-        // stub
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.player, menu);
+        getMenuInflater().inflate(R.menu.library, menu);
         return true;
     }
 
